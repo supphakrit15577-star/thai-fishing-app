d6d75dbd79c33a9768082e351c80dc95903cd20e
supphakrit15577-star <supphakrit15577@gmail.com>
2026-01-16 16:16:24 +0700

    adjusi map 550

--- fishing_app.py
+++ fishing_app.py
@@ -160,6 +160,6 @@ def render_map(df):
         ).add_to(m)
 
     # แสดงผลแผนที่กว้างเต็มจอ (Height ปรับตามความเหมาะสมของอุปกรณ์)
-    st_folium(m, width="100%", height=600, returned_objects=[], key="stable_sidebar_map")
+    st_folium(m, width="100%", height=550, returned_objects=[], key="stable_sidebar_map")
 
 render_map(df)
